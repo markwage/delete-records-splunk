e25bf20fb9a8e674d927663faa4840eca3857b4f
markwage <mark.wage@hotmail.com>
2019-02-13 13:44:45 +0100

    Reading the splunk debug logfile and remove certain records

---
+++ readLog.py
@@ -0,0 +1,43 @@
+#===========================================================
+# import time
+#===========================================================
+
+from datetime import datetime
+
+strInputFile = "C:\Users\Mark\OneDrive\Documenten\eclipse-workspace/im006222113Debug.txt"
+strOutputFile = "im006222113DebugFiltered.txt"
+strOutputFileRemoved = "im006222113DebugRemoved.txt"
+
+whatToDelete = ["MDC", "CookieFactory", "MultiFunctionsLoadMonitor"]
+
+currentTime = datetime.now()
+print ("%s Start met inlezen van de file" % currentTime)
+
+with open(strInputFile) as log, open(strOutputFile, "w") as outputFile, open(strOutputFileRemoved, "w") as outputFileRemoved :
+    
+    for line in log:
+        for toDelete in whatToDelete:
+        #if "MDC" in line or "CookieFactory" in line or "MultiFunctionsLoadMonitor" in line or line.count(';') <=2:
+            if toDelete in line:
+                outputFileRemoved.write(line)
+                break
+            else:
+                outputFile.write(line)
+                break
+            #if line.count(';') != 15:
+                #print (line)
+                #print (line.count(';'))
+
+currentTime = datetime.now()
+print ("%s File is gefilterd en weggeschreven" % currentTime)
+
+#==========================================================================            
+# Count number of lines in the files to see how many records are deleted
+#==========================================================================
+
+numLinesInput = sum(1 for line in open(strInputFile))
+numLinesOutput = sum(1 for line in open(strOutputFile))
+numLinesRemoved = sum(1 for line in open(strOutputFileRemoved))
+print ("Aantal inputrecords : %d" % numLinesInput)
+print ("Aantal outputrecords: %d" % numLinesOutput)
+print ("Totaal aantal lines removed: %d" % numLinesRemoved)
