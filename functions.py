e406cad3ebb62fb46f7135bb58fecb5d8e994f00
markwage <mark.wage@hotmail.com>
2019-03-06 09:27:12 +0100

    Nogmaals een test met Git

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ from datetime import datetime
 #=================================================
 # Function to write logrecord.
 # 06-03 08:45 Test met git
+# 06-03 09:26 Nogmaals een test
 #=================================================
 def writeLogRecord(arg1, arg2):
     currentTime = datetime.now()
